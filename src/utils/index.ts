3aef02bc3df71b3680e28f98eaeb1515752f83eb
Heru Gunawan <b0ndheru1@gmail.com>
2023-12-24 11:27:51 +0700

    chore: small changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,15 @@
+import clsx from 'clsx'
+import { ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function truncateText(text: string, length: number) {
 	let truncated = text.slice(0, length + 1)
 	if (truncated.length === length + 1) {
 		truncated += '...'
 	}
 	return truncated
+}
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs))
 }
